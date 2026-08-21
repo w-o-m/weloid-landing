5b8d2f8a6c660ed662a59e3b36b651944c1d027a
mrprotocoll <lekanvgbg@gmail.com>
2026-08-21 08:41:17 +0100

    udate

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,28 +10,33 @@ const CONSTANTS = {
   slogan: "Forensic Software Engineering",
   description:
     "Every broken system has a story. We find it. Then we rewrite the ending. Weloid builds, rescues and investigates software.",
-  email: "hello@example.com",
+  email: "info@weloid.com",
+  tel: "+27 60 432 5750",
   socials: [
     {
       name: "LinkedIn",
-      handle: "/company/weloid",
-      url: "https://www.linkedin.com/company/weloid",
+      handle: "weloid-corp",
+      url: "https://www.linkedin.com/in/weloid-corp/",
     },
     {
       name: "X",
-      handle: "@weloid",
-      url: "https://x.com/weloid",
+      handle: "@weloidic",
+      url: "https://x.com/weloidinc",
     },
     {
       name: "Instagram",
       handle: "@weloid",
       url: "https://www.instagram.com/weloid",
+    },{
+      name: "Facebook",
+      handle: "@weloid",
+      url: "https://www.facebook.com/weloid",
     },
-    {
-      name: "GitHub",
-      handle: "/weloid",
-      url: "https://github.com/weloid",
-    },
+    // {
+    //   name: "GitHub",
+    //   handle: "/weloid",
+    //   url: "https://github.com/weloid",
+    // },
   ],
   navigation: {
     left: [
